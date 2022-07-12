fd0971b06824f5f0118fd0709d4f6c77b36228d7
ArandomDev <haow6449@gmail.com>
2022-07-12 12:26:55 -0400

    Publish with new arguments, thanks @danzimm

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='dyldextractor',
-	version='2.1.0',
+	version='2.1.1',
 	description='Extract Binaries from Apple\'s Dyld Shared Cache',
 	long_description='file: README.md',
 	long_description_content_type='text/markdown',
