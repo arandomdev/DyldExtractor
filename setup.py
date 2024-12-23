2a4dea0ad11db2ac3cda93d463e81963906e9eb9
Donato Fiore <donato.v.fiore@gmail.com>
2024-12-23 17:23:42 -0500

    Revert URL in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 	long_description_content_type='text/markdown',
 	python_requires='>=3.8',
 	author='arandomdev',
-	url='https://github.com/donato-fiore/dyldextractor',
+	url='https://github.com/arandomdev/dyldextractor',
 	install_requires=['progressbar2', 'capstone==4.0.2'],
 	packages=find_packages(
 		where='src'
